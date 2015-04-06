dc8f6464bd8017bbb58f1e102c60e47a9a61b987
Arka Ganguli <arkaganguli1@gmail.com>
2015-04-06 18:47:01 -0400

    adding username functionality

--- public/client.js
+++ public/client.js
@@ -1,10 +1,33 @@
 var socket = io();
+<<<<<<< Updated upstream
 $('form').submit(function(){
+=======
+
+$('#submit-username').on('click',function(e){
+	e.preventDefault();
+	socket.emit('new user', $('#enter-name').val() + " has entered the chat room");
+	//should check here later on if the username already exists or not
+	$('#container').show();
+	$('#username-container').hide();
+	//reset 
+	$('#enter-name').val('');
+	return false;
+});
+//submit a message
+$('#submit-server').on('click',function(e){
+	e.preventDefault;
+>>>>>>> Stashed changes
 	socket.emit('chat message', $('#message').val());
 	//set text field back to default
 	$('#message').val('');
 	return false;
 });
+
+//add message to chat div
 socket.on('chat message', function(msg){
 	$('#msglist').append($('<li>').text(msg));
+});
+
+socket.on('new user', function(name){
+	$('#msglist').append($('<li>').text(name));
 });
--- server.js
+++ server.js
@@ -9,22 +9,30 @@ server.listen(3000);
 console.log("Server running on  port 3000");
 
 app.use(express.static(__dirname + '/public'));
+
 //routes
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-//users connected
-var users = {};
-var numUsers = 0;
-
 io.on('connection', function(socket){
 	console.log('a user connected');
-	// socket.on('disconnect', function(socket){
-	// 	console.log('user disconnected');
-	// });
+
+	//user disconnect
+	socket.on('disconnect', function(socket){
+		console.log('user disconnected');
+	});
+
+	//new user logs in
+	socket.on('new user', function(name){
+		console.log('name: ' + name);
+		io.emit('new user', name);
+	});
+
+	//new chat message sent
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
+		//send to everyone
 		io.emit('chat message', msg);
 	});
 });
