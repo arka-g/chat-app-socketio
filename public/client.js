b7b70cd8f2805c4ac931c3e7a91b125ae9b18075
Arka Ganguli <arkaganguli1@gmail.com>
2015-04-06 14:04:32 -0400

    basic chat app implementation

---
+++ public/client.js
@@ -0,0 +1,11 @@
+var socket = io();
+$('form').submit(function(){
+	socket.emit('chat message', $('#message').val());
+	//set text field back to default
+	$('#message').val('');
+	return false;
+});
+
+socket.on('chat message', function(msg){
+	$('#msglist').append($'<li>').text(msg);
+});
--- server.js
+++ server.js
@@ -20,4 +20,12 @@ var numUsers = 0;
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+	// socket.on('disconnect', function(socket){
+	// 	console.log('user disconnected');
+	// });
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
 });
+
