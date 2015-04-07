1123769bf595380141bc8fafdadc29f415b225e9
Arka Ganguli <arkaganguli1@gmail.com>
2015-04-07 02:09:12 -0400

    We have usernames!

--- public/client.js
+++ public/client.js
@@ -1,9 +1,10 @@
 var socket = io();
 $('#submit-username').on('click',function(e){
 	e.preventDefault();
-	socket.emit('new user', $('#enter-name').val() + " has entered the chat room");
+	socket.emit('new user', $('#enter-name').val());
 	//should check here later on if the username already exists or not
 	$('#container').show();
+	$('#users').show();
 	$('#username-container').hide();
 	//reset 
 	$('#enter-name').val('');
@@ -20,9 +21,16 @@ $('#submit-server').on('click',function(e){
 
 //add message to chat div
 socket.on('chat message', function(msg){
-	$('#msglist').append($('<li>').text(msg));
+	$('#msglist').append($('<li>').text(msg.username + ": " + msg.message));
 });
 
 socket.on('new user', function(name){
 	$('#msglist').append($('<li>').text(name));
+});
+
+socket.on('usernames', function(usernames){
+	//loop through usernames array and print
+	for(i = 0; i<usernames.length;i++){
+		$('#userslist').append($('<li>').text(usernames[i]));
+	}
 });
--- server.js
+++ server.js
@@ -15,25 +15,43 @@ app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+//users connected
+usernames =[];
+
 io.on('connection', function(socket){
 	console.log('a user connected');
 
 	//user disconnect
 	socket.on('disconnect', function(socket){
 		console.log('user disconnected');
+		var index = usernames.indexOf(socket.username);
+		//remove username from array
+		usernames.splice(index,1);
+		//update usernames
+		io.emit('usernames', usernames);
+
+		//no username
+		if(!socket.username){
+			return;
+		}
 	});
 
 	//new user logs in
 	socket.on('new user', function(name){
 		console.log('name: ' + name);
-		io.emit('new user', name);
+		//bind to socket of user
+		socket.username = name;
+		//add to array
+		usernames.push(socket.username);
+		io.emit('new user', name + " has joined the chat room");
+		io.emit('usernames', usernames);
 	});
 
 	//new chat message sent
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
 		//send to everyone
-		io.emit('chat message', msg);
+		io.emit('chat message', {message: msg, username: socket.username});
 	});
 });
 
