3c0339ee552cb748f4e51cf51901aea143f0aa19
Arka Ganguli <arkaganguli1@gmail.com>
2015-04-07 13:27:15 -0400

    slight change

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ io.on('connection', function(socket){
 		io.emit('chat message', {message: msg, username: socket.username});
 	});
 
+	//user disconnect
 	socket.on('disconnect', function(){
 		//console.log(usernames+ ' disconnected');
 		var index = usernames.indexOf(socket.username);
